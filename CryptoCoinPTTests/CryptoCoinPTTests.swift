52b2962e17a29eec3c6eabef8b3aa5f0662c02f5
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-12 20:05:43 +0100

    Changes after life interacting with Jippy in class

---
+++ CryptoCoinPTTests/CryptoCoinPTTests.swift
@@ -0,0 +1,29 @@
+// Created 09.11.2023 
+
+import XCTest
+
+final class CryptoCoinPTTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+        let x = 5
+        
+        XCTAssertTrue(x % 2 == 0, "Ist \(x) gerade")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
